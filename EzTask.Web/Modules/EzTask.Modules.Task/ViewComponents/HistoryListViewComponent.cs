80b7c528c165b684c13a8d8797d53d255737d6b3
duyli <duyli@DESKTOP-0U64O8Q>
2018-08-14 22:22:13 +0700

    update

---
+++ EzTask.Web/Modules/EzTask.Modules.Task/ViewComponents/HistoryListViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using EzTask.Business;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EzTask.Modules.Task.ViewComponents
+{
+    public class HistoryListViewComponent :ViewComponent
+    {
+        protected EzTaskBusiness EzTask;
+
+        public HistoryListViewComponent(EzTaskBusiness business)
+        {
+            EzTask = business;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int taskId)
+        {
+            var data = await EzTask.Attachment.GetAttachments(taskId);
+            return View(data);
+        }
+    }
+}
