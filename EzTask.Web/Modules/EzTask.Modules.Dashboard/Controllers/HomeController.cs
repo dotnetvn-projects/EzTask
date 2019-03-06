1d2e50fec9a52b29bd5e1b5206f3acac11f54f36
ASV_User2 <ASV_User2@ASV_USER2-PC>
2019-03-06 17:30:07 +0700

    update

--- EzTask.Web/Modules/EzTask.Modules.Dashboard/Controllers/HomeController.cs
+++ EzTask.Web/Modules/EzTask.Modules.Dashboard/Controllers/HomeController.cs
@@ -2,10 +2,6 @@
 using Microsoft.AspNetCore.Mvc;
 using EzTask.Modules.Core.Controllers;
 using EzTask.Web.Framework.Attributes;
-using EzTask.Plugin.MessageService;
-using EzTask.Plugin.MessageService.Data.Email;
-using System.Threading.Tasks;
-using EzTask.Modules.Dashboard.ViewModels;
 
 namespace EzTask.Modules.Dashboard.Controllers
 {
